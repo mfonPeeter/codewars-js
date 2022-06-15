087ce3acbd72b85abf7c965acd559eac9193c332
MfonobongGodwin <godimfon@gmail.com>
2022-06-15 16:58:49 +0100

    Square (n) Sum

---
+++ 8 kyu/square(n)sum.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers) {
+  let total = 0;
+  for (const number of numbers) {
+    const sum = number ** 2;
+    total += sum;
+  }
+  return total;
+}
+
+console.log(squareSum([1, 2, 2], 5));
+
+// Best Practices
+function squareSum(numbers) {
+  return numbers.reduce(function (sum, n) {
+    return n * n + sum;
+  }, 0);
+}
+
+console.log(squareSum([1, 2, 2], 5));
