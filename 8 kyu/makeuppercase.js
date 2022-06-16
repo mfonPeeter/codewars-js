4045edfad6acf43cff9e54ddaa1c450c47ce14c1
MfonobongGodwin <godimfon@gmail.com>
2022-06-16 23:28:02 +0100

    MakeUpperCase

---
+++ 8 kyu/makeuppercase.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello"));
