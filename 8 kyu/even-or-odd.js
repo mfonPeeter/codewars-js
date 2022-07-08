c95a91ac0b0873d8ff0f080974a17ad3bf3237ca
MfonobongGodwin <godimfon@gmail.com>
2022-07-08 12:57:46 +0100

    Even or Odd

---
+++ 8 kyu/even-or-odd.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(even_or_odd(2));
+console.log(even_or_odd(7));
+console.log(even_or_odd(-42));
