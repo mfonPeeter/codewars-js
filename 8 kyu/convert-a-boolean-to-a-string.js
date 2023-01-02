4927ff2c28518c794de06b8e54640b811209f323
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-01-02 14:31:30 +0100

    Convert a boolean to a string

---
+++ 8 kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,10 @@
+"use script";
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+const booleanToString = (b) => b.toString();
+
+console.log(booleanToString(true));
+console.log(booleanToString(false));
